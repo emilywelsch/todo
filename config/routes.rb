8e98707a6ca9cf4c36f0a5cf8fe6c7543fe9a66d
emilywelsch <emwelsch@gmail.com>
2018-09-22 11:58:35 -0400

    Removed site controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'site/index'
+  resources :lists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'site#index'
+root 'lists#index'
 end
